b4b8a899102e5df0f436168e21fe568916f7d5f0
Denis Syshkov <ga.akam@yandex.ru>
2023-01-25 19:13:29 +0300

    flake8

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -67,12 +67,12 @@ WSGI_APPLICATION = 'api_yamdb.wsgi.application'
 
 DATABASES = {
     'default': {
-	    'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.postgresql'),
-	    'NAME': os.getenv('DB_NAME', default='postgres'),
-	    'USER': os.getenv('POSTGRES_USER', default='postgres'),
-	    'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='postgres'),
-	    'HOST': os.getenv('DB_HOST', default='db'),
-	    'PORT': os.getenv('DB_PORT', default='5432')
+        'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.postgresql'),
+        'NAME': os.getenv('DB_NAME', default='postgres'),
+        'USER': os.getenv('POSTGRES_USER', default='postgres'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='postgres'),
+        'HOST': os.getenv('DB_HOST', default='db'),
+        'PORT': os.getenv('DB_PORT', default='5432')
     }
 }
 
@@ -114,7 +114,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 
-MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 AUTH_USER_MODEL = 'users.User'
 
